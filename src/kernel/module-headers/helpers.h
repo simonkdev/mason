ae87e0fa1fd926d4a41a7c94338cd1d23a75eaa7
simonkdev <simonkorten09@gmail.com>
2026-03-11 09:27:57 +0100

    Add helpers.h

---
+++ src/kernel/module-headers/helpers.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "../module-headers/terminal.h"
+
+
+
+char* convert_to_base(int value, char* result, int base)
+size_t strlen(const char* str)
