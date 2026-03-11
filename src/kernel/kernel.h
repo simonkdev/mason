5ea012edb5a640d936bff8ba7fcfb78844e54f55
simonkdev <simonkorten09@gmail.com>
2026-03-11 09:27:57 +0100

    Add kernel.h

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,23 +1,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "module-headers/helpers.h"
+#include "module-headers/terminal.h"
 
-#define VGA_WIDTH 80
-#define VGA_HEIGHT 25
-#define VGA_MEMORY 0xB8000
-
-extern size_t terminal_row;
-extern size_t terminal_column;
-extern uint8_t terminal_color;
-extern uint16_t* terminal_buffer;
-
-void terminal_initialize(void);
-void terminal_setcolor(uint8_t color);
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
-void terminal_putchar(char c);
-void terminal_write(const char* data, size_t size);
-void terminal_writestring(const char* data);
-void terminal_writeint(int value, char* result, int base);
-void terminal_write_u64(uint64_t value, int base, bool add_prefix, bool uppercase);
 
 void kernel_main(void);
