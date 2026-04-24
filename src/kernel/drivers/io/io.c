4d9666b94c2d7f88a9293fae98fe0f6f87aadc71
simonkdev <simonkorten09@gmail.com>
2026-04-24 12:40:18 +0200

    Add io.c

--- src/kernel/drivers/io/io.c
+++ src/kernel/drivers/io/io.c
@@ -1,13 +1,16 @@
-#include "../module-headers/io.h"
+#include "io.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-void outb(uint16_t port, uint8_t value)
+void outb(uint16_t port, uint8_t value) 
 {
     __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
 
-uint8_t intb(uint16_t port)
+uint8_t inb(uint16_t port) 
 {
     uint8_t ret;
-    __asm__ volative("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
