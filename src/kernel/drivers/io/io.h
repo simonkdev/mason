99096db2363a47930aa92c2f1ec31bd1a3e7a90a
simonkdev <simonkorten09@gmail.com>
2026-04-24 12:40:18 +0200

    Add io.h

--- src/kernel/drivers/io/io.h
+++ src/kernel/drivers/io/io.h
@@ -1,2 +1,7 @@
-uint8_t intb(uint16_t port)
-void outb(uint16_t port, uint8_t value)
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+uint8_t inb(uint16_t port);
+void outb(uint16_t port, uint8_t value);
+
