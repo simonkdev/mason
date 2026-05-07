c46c166b0032f50206cd337ed610d618744c4eb5
simonkdev <simonkorten09@gmail.com>
2026-05-07 12:23:09 +0200

    Add keyboard.h

--- src/kernel/drivers/io/keyboard.h
+++ src/kernel/drivers/io/keyboard.h
@@ -2,8 +2,12 @@
 #include "vga.h"
 #include "io.h"
 
+extern int modifier_flags[3];
 
 void send_signal_to_ps2(uint8_t signal);
 uint8_t read_signal_from_ps2();
 int get_current_code_set();
+int send_echo(void);
+int initialize_keyboard(void);
+char* main_loop(void);
 // uint8_t convert_code_set_hex_to_int(uint8_t reply);
