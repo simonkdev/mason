1063bd5aec7315668466126a293b3ad15a50e28e
simonkdev <simonkorten09@gmail.com>
2026-05-07 12:23:09 +0200

    Add helpers.c

--- src/kernel/helpers.c
+++ src/kernel/helpers.c
@@ -48,7 +48,8 @@ void do_nothing()
 
 }
 
-int strcmp(const char* s1, const char* s2) {
+int strcmp(const char* s1, const char* s2) 
+{
     while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
         s1++;
         s2++;
