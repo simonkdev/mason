00f17098a6331bf4ca240438c0ca9fddfeb0da3d
simonkdev <simonkorten09@gmail.com>
2026-07-24 18:59:50 +0200

    added hardware interrupt stubs (timer and keyboard only for now)

--- src/kernel/interrupts/isr.s
+++ src/kernel/interrupts/isr.s
@@ -2,7 +2,6 @@
 
 .global interrupt_stub_30
 .type interrupt_stub_30, @function
-.extern testHandler
 
 
 interrupt_stub_30:
@@ -12,6 +11,30 @@ interrupt_stub_30:
 
     # every push has its own negative equivalent: iret undoes the interrupt, pusha gets undone by popa, our add and push cancel each other out.
 .size interrupt_stub_30, . - interrupt_stub_30
+
+.global interrupt_stub_21
+.type interrupt_stub_21, @function
+
+
+interrupt_stub_21:
+    push $0x21
+    jmp interrupt_handler
+
+
+    # every push has its own negative equivalent: iret undoes the interrupt, pusha gets undone by popa, our add and push cancel each other out.
+.size interrupt_stub_21, . - interrupt_stub_21
+
+.global interrupt_stub_20
+.type interrupt_stub_20, @function
+
+
+interrupt_stub_20:
+    push $0x20
+    jmp interrupt_handler
+
+
+    # every push has its own negative equivalent: iret undoes the interrupt, pusha gets undone by popa, our add and push cancel each other out.
+.size interrupt_stub_20, . - interrupt_stub_20
 
 
 .global interrupt_handler
